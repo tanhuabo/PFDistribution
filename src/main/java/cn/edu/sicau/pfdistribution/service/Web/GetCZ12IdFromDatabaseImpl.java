840e661326d3dd4d67201706762d4c6724a579e1
平才 <928689419@qq.com>
2019-11-16 18:04:31 +0800

    BY LiYongPing
    Signed-off-by: 平才 <928689419@qq.com>

--- src/main/java/cn/edu/sicau/pfdistribution/service/Web/GetCZ12IdFromDatabaseImpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/service/Web/GetCZ12IdFromDatabaseImpl.java
@@ -26,33 +26,42 @@ public class GetCZ12IdFromDatabaseImpl implements GetCZ12IdFromDatabase {
     @Override
     public Map getCZ12IDNoParameterFromDatabase() {
         Map result = new HashMap();
-        List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\"");
-        Iterator it = rows.iterator();
-        while(it.hasNext()) {
-            Map userMap = (Map) it.next();
-            Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
-            Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
-            String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
-            Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
-            result.put(CZ12_ID,SectionID);
+        try {
+            List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\"");
+            Iterator it = rows.iterator();
+            while(it.hasNext()) {
+                Map userMap = (Map) it.next();
+                Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
+                Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
+                String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
+                Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
+                result.put(CZ12_ID,SectionID);
+            }
+        }catch (Exception e){
+            result.put("0",0);
         }
+
         return result;
     }
 
     @Override
     public Map<String,Integer> getCZ12IDWithParameterFromDatabase(Integer id){
-        List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\" where QJ_ID ="+id+"");
-        Iterator it = rows.iterator();
         Map result = new HashMap();
-        while (it.hasNext()){
-            Map userMap = (Map) it.next();
-            Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
-            if (SectionID.equals(id)){
-                Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
-                Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
-                String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
-                result.put(CZ12_ID,SectionID);
-            }
+        try {
+            List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\" where QJ_ID ="+id+"");
+            Iterator it = rows.iterator();
+            while (it.hasNext()){
+                Map userMap = (Map) it.next();
+                Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
+                if (SectionID.equals(id)){
+                    Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
+                    Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
+                    String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
+                    result.put(CZ12_ID,SectionID);
+                }
+         }
+        }catch (Exception e){
+            result.put("0",0);
         }
         return result;
     }

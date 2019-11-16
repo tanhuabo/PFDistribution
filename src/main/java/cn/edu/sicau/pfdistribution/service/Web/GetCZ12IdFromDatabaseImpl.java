73552e568206bde0c113d9a766209e839ef48b80
平才 <928689419@qq.com>
2019-11-16 19:55:13 +0800

    BY LiYongPing
    Signed-off-by: 平才 <928689419@qq.com>

--- src/main/java/cn/edu/sicau/pfdistribution/service/Web/GetCZ12IdFromDatabaseImpl.java
+++ src/main/java/cn/edu/sicau/pfdistribution/service/Web/GetCZ12IdFromDatabaseImpl.java
@@ -13,8 +13,8 @@ import java.util.Map;
 
 /**
  * @author weiyongzhao
- *return 两个车站id和区间id的map
- * */
+ * return 两个车站id和区间id的map
+ */
 
 @Repository
 public class GetCZ12IdFromDatabaseImpl implements GetCZ12IdFromDatabase {
@@ -27,41 +27,41 @@ public class GetCZ12IdFromDatabaseImpl implements GetCZ12IdFromDatabase {
     public Map getCZ12IDNoParameterFromDatabase() {
         Map result = new HashMap();
         try {
-            List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\"");
+            List rows = jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\"");
             Iterator it = rows.iterator();
-            while(it.hasNext()) {
+            while (it.hasNext()) {
                 Map userMap = (Map) it.next();
                 Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
-                Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
-                String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
+                Integer CZ2_ID = Integer.parseInt(userMap.get("CZ2_ID").toString());
+                String CZ12_ID = CZ1_ID.toString() + " " + CZ2_ID.toString();
                 Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
-                result.put(CZ12_ID,SectionID);
+                result.put(CZ12_ID, SectionID);
             }
-        }catch (Exception e){
-            result.put("0",0);
+        } catch (Exception e) {
+            result.put("0", 0);
         }
 
         return result;
     }
 
     @Override
-    public Map<String,Integer> getCZ12IDWithParameterFromDatabase(Integer id){
+    public Map<String, Integer> getCZ12IDWithParameterFromDatabase(Integer id) {
         Map result = new HashMap();
         try {
-            List rows=jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\" where QJ_ID ="+id+"");
+            List rows = jdbcTemplate.queryForList("SELECT CZ1_ID,CZ2_ID,QJ_ID FROM \"SCOTT\".\"dic_section\" where QJ_ID =" + id + "");
             Iterator it = rows.iterator();
-            while (it.hasNext()){
+            while (it.hasNext()) {
                 Map userMap = (Map) it.next();
                 Integer SectionID = Integer.parseInt(userMap.get("QJ_ID").toString());
-                if (SectionID.equals(id)){
+                if (SectionID.equals(id)) {
                     Integer CZ1_ID = Integer.parseInt(userMap.get("CZ1_ID").toString());
-                    Integer  CZ2_ID =Integer.parseInt(userMap.get("CZ2_ID").toString()) ;
-                    String CZ12_ID=CZ1_ID.toString()+" "+CZ2_ID.toString();
-                    result.put(CZ12_ID,SectionID);
+                    Integer CZ2_ID = Integer.parseInt(userMap.get("CZ2_ID").toString());
+                    String CZ12_ID = CZ1_ID.toString() + " " + CZ2_ID.toString();
+                    result.put(CZ12_ID, SectionID);
                 }
-         }
-        }catch (Exception e){
-            result.put("0",0);
+            }
+        } catch (Exception e) {
+            result.put("0", 0);
         }
         return result;
     }
